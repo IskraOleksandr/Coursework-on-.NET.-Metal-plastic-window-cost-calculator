033cc02ada4c6fc27e22d9eb35fadc506cd465bd
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2024-02-28 17:39:49 +0200

    Добавьте файлы проекта.

---
+++ Metal-plastic window cost calculator/Form1.cs
@@ -0,0 +1,10 @@
+namespace Metal_plastic_window_cost_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
